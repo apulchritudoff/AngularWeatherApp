6003e445a8563bcdc80d06228abcc0dabdf28ea0
apulchritudoff <32424649+apulchritudoff@users.noreply.github.com>
2022-05-16 09:30:38 -0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '4554ee4b63msh5cdfbc18595d26fp1bb59ajsn049463d06640'
 };
 
 /*
